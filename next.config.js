5467126569a907a089fc6c22dd536aa212f62ca1
Aymane Droussi <60798100+aymanedroussi@users.noreply.github.com>
2022-03-02 13:51:24 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   
   reactStrictMode: true,
   env: {
-    MONGODB_URI: "mongodb://localhost:27017/dossierpatient",
+    MONGODB_URI: "mongodb+srv://adev:adev@cluster0.7yy55.mongodb.net/dossierpatient?retryWrites=true&w=majority",
     SECRET:"LfsZWKdsKRwhz5bS7YOEI0A564a37saF",
     server:"http://localhost:3001" // edit alse the config file ' server ' 
     
