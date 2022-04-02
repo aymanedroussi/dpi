af3c44db7aa59a863417cf03d3591b2cd86b422e
taha_f <84233310+madad-tech@users.noreply.github.com>
2022-04-02 00:07:51 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,9 @@ module.exports = {
   reactStrictMode: true,
   env: {
     //MONGODB_URI: "mongodb://localhost:27017/dossierpatient?retryWrites=true&w=majority",
-	//MONGODB_URI: "mongodb+srv://new_user:S53oJfV3i38n2Jki@cluster0.i52k8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+	//MONGODB_URI: "mongodb+srv://new_user:S53oJfV3i38n2Jki@cluster0.i52k8.mongodb.net/dossierpatient?retryWrites=true&w=majority",
     SECRET:"LfsZWKdsKRwhz5bS7YOEI0A564a37saF",
     server:"http://localhost:3001", // edit alse the config file ' server ' 
   },
   
-
-//mongodb://localhost:27017/dossierpatient
-  //mongodb+srv://adev:adev@cluster0.nphce.mongodb.net/dossierpatient?retryWrites=true&w=majority
 }
